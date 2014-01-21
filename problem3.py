4bf6a1e004ab0822f1a22f9e287066aa33ab3554
d.gabaydulin <D.Gabaydulin@qiwi.ru>
2014-01-21 20:36:42 +0400

    * faster way to find max prime factor in the problem 3

--- problem3.py
+++ problem3.py
@@ -1,65 +1,21 @@
 
-def xlongrange(start, limit, step):
-	n = start
+import math
 
-	if n < limit:
-		while n < limit:
-			yield n
-			n += step
-	else:
-		while n > limit:
-			yield n
-			n += step
+def getLargestPrime(max):
 
-def isPrimeNumber(number):
-	if number % 2 == 0 or number % 3 == 0:
-		return False
+	prime = 2
+	x = 2
+	while x <= max:
+		if max % x == 0:
+			prime = x
+			max /= x
+			x = x - 1
+		else:
+			x = x + 1
 
-	k = 6
-	while ((k - 1) * (k - 1)) <= number:
-		if number % (k - 1) == 0 or number % (k + 1) == 0:
-			return False;
+	return prime
+			
+print getLargestPrime(600851475143)
 
-		k += 6
-	
-	return True
-
-def findMaxFactor(number):
-	maxFactor = None
-	for elt in xlongrange(number, 2, -1):
-		if elt % 2 == 0 or elt % 3 == 0:
-			continue
-
-		if number % elt == 0:
-			if isPrimeNumber(elt):
-				return elt
-	
-		#print elt
-	return None
-				
- 
-#print findMaxFactor(13195)
-#print findMaxFactor(600851475143)
-original = 600851475143
-limit = 600851475143 / 2
-
-curr = limit
-while curr > 0:
-	if original % curr == 0:
-		print curr
-		if isPrimeNumber(curr):
-			print curr
-			break
-
-	curr = curr - 1
-
-#for elt in xlongrange(limit, 2, -1):
-#	print elt
-#	if original % elt == 0:
-#		print elt
-		#if isPrimeNumber(elt):
-		#	print elt
-#	else:
-#		limit = elt / 2
 
 
